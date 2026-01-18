48cfaf91cd99cea88eaa17794ad690096901c4db
agung_win_22 <134590738+agungwin22@users.noreply.github.com>
2026-01-18 11:10:19 +0700

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo("password", message="Password tidak sama!")])
+    submit = SubmitField('Daftar')
