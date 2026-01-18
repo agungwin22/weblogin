354c753e0017e0f605249ac543650e6084911484
agung_win_22 <134590738+agungwin22@users.noreply.github.com>
2026-01-18 11:09:15 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, flash, url_for
-from app.forms import LoginForm
+from app.auth.forms import LoginForm, RegisterForm
 
 main = Blueprint('main', __name__)
 
@@ -16,4 +16,13 @@ def login():
             flash("Login Berhasil!!", "success")
         else:
             flash("Email & Password Salah", "danger")
-    return render_template('login.html', form=form)
+    return render_template('auth/login.html', form=form)
+
+@main.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        # simulasi simpan user (belum DB)
+        flash("Registrasi berhasil!. silahkan login", "success")
+        return redirect(url_for('main.login'))
+    return render_template('auth/register.html', form=form)
